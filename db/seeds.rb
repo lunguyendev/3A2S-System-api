2d081790b473540fb41b9c6b91dccba2614d472b
thanhlong-dev <nguyen.thanh.long@sun-asterisk.com>
2021-04-01 13:44:57 +0700

    join Event

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 include Util::Generation
 
 puts "Start"
 password = generate_hash_password("admin")
-lecture = Lecturer.create(email: "admin@gmail.com",hashed_password: password)
+Lecturer.create(email: "admin@gmail.com", hashed_password: password)
 
 puts "End"
