7baa742f763d67e471b90f0a92021240f896bace
thanhlong-dev <nguyen.thanh.long@sun-asterisk.com>
2021-05-23 17:27:32 +0700

    API profile

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -4,6 +4,13 @@ class Api::V1::UserSerializer < ActiveModel::Serializer
   attributes \
     :uid,
     :email,
+    :name,
     :type,
-    :avatar
+    :avatar,
+    :phone,
+    :gender,
+    :birthday,
+    :class_activity,
+    :id_student,
+    :id_lecturer
 end
