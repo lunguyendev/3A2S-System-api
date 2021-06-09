2c7d111c26b89d7f675be85296e67009cba2875f
thanhlong-dev <nguyen.thanh.long@sun-asterisk.com>
2021-06-10 04:02:14 +0700

    Suggestion

--- app/services/api/event/statistics/type_event.rb
+++ app/services/api/event/statistics/type_event.rb
@@ -4,12 +4,15 @@ class Api::Event::Statistics::TypeEvent
   def execute
     type_events = Suggestion.group(:type_event_uid).count
     total = Suggestion.count
-    data = []
+    type_event = []
+    values = []
     type_events.each do |key, value|
-      data << {
-        "#{TypeEvent.find(key).name}": (value/total.to_f * 100).round(2)
-      }
+      type_event << "#{TypeEvent.find(key).name}"
+      values << (value/total.to_f * 100).round(2)
     end
-    data
+    {
+      type_event: type_event,
+      value: values
+    }
   end
 end
