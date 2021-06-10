e1cee237e8f92795f9202fe915fc2e182cc0fe51
thanhlong-dev <thanhlongdn.it@gmail.com>
2021-06-10 12:39:03 +0700

    fix_type

--- app/services/api/event/statistics/type_event.rb
+++ app/services/api/event/statistics/type_event.rb
@@ -8,19 +8,11 @@ class Api::Event::Statistics::TypeEvent
     values = []
     type_events.each do |key, value|
       type_event << "#{TypeEvent.find(key).name}"
-<<<<<<< HEAD
       values << (value/total.to_f * 100).round(2)
     end
     {
       type_event: type_event,
       value: values
-=======
-      value << (value/total.to_f * 100).round(2)
-    end
-    {
-      type_event: type_event,
-      value: value
->>>>>>> fix_type_event
     }
   end
 end
